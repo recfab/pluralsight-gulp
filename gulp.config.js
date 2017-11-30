bedf7d46791401e3876e3f3fa042be6cdeb8cf88
Yael Namen <yael.namen@wizards.com>
2017-11-30 14:28:14 -0800

    clean-styles task

--- gulp.config.js
+++ gulp.config.js
@@ -1,9 +1,20 @@
+var path = require("path");
+
 module.exports = function () {
+    var client = "./src/client";
+
     var config = {
+        temp: "./.tmp",
+
+        /**
+         * File paths
+         */
         alljs: [
             "./src/**/*.js",
             "./*.js"
-        ]
+        ],
+
+        less: path.join(client, "styles", "styles.less")
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require("gulp");
 var args = require("yargs").argv;
+var del = require("del");
+var path = require("path");
 
 var config = require("./gulp.config")();
 
@@ -7,6 +9,7 @@ var config = require("./gulp.config")();
 // e.g. $.if will load "gulp-if"
 var $ = require("gulp-load-plugins")({ lazy: true });
 
+// So we don't need these lines
 // var jshint = require("gulp-jshint");
 // var jscs = require("gulp-jscs");
 // var util = require("gulp-util");
@@ -26,7 +29,28 @@ gulp.task("vet", () => {
     .pipe($.jshint.reporter("fail"));
 });
 
+gulp.task("styles", ["clean-styles"], () => {
+    log("Compiling LESS --> CSS");
+    log("    path: " + config.less);
+
+    return gulp
+        .src(config.less)
+        .pipe($.less())
+        .pipe($.autoprefixer({ browsers: ["last 2 version", "> 5%"]}))
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task("clean-styles", (done) => {
+    var files = path.join(config.temp, "**/*.css");
+    return clean(files, done);
+});
+
 //////////////
+
+function clean(path) {
+    log("Cleaning: " + $.util.colors.blue(path));
+    return del(path);
+}
 
 function log(msg) {
     if (typeof(msg) === "object") {
