df1e1d18cc2c52b8e37ed919958dd5f25d591a1c
Yael Namen <yael.namen@wizards.com>
2017-11-30 13:55:38 -0800

    finish module: Code analysis with JSHint and JSCS

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,42 @@
 var gulp = require("gulp");
-var jshint = require("gulp-jshint");
-var jscs = require("gulp-jscs");
+var args = require("yargs").argv;
+
+var config = require("./gulp.config")();
+
+// load plugins as needed
+// e.g. $.if will load "gulp-if"
+var $ = require("gulp-load-plugins")({ lazy: true });
+
+// var jshint = require("gulp-jshint");
+// var jscs = require("gulp-jscs");
+// var util = require("gulp-util");
+// var gulpprint = require("gulp-print");
+// var gulpif = require("gulp-if");
 
 gulp.task("vet", () => {
+    log("Analyzing source with JSHint and JSCS");
+
     return gulp
-    .src([
-        "./src/**/*.js",
-        "./*.js"
-    ])
-    .pipe(jscs())
-    .pipe(jscs.reporter("jscs-stylish"))
-    .pipe(jshint())
-    .pipe(jshint.reporter("jshint-stylish", { verbose: true }));
+    .src(config.alljs)
+    .pipe($.if(args.verbose, $.print()))
+    .pipe($.jscs())
+    .pipe($.jscs.reporter("jscs-stylish"))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter("jshint-stylish", { verbose: true }))
+    .pipe($.jshint.reporter("fail"));
 });
+
+//////////////
+
+function log(msg) {
+    if (typeof(msg) === "object") {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    }
+    else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
